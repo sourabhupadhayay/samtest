15ddcde2a3cb121760f08c3e830545e896f8257a
Kalph upadhyay <kalpupadhyay@gmail.com>
2022-06-14 21:09:53 +0530

    fixed performance issue

--- src/app/pages/auth-module/verify-otp/verify-otp.page.ts
+++ src/app/pages/auth-module/verify-otp/verify-otp.page.ts
@@ -1,4 +1,11 @@
-import { Component, OnInit } from "@angular/core";
+import {
+  ChangeDetectionStrategy,
+  ChangeDetectorRef,
+  Component,
+  NgZone,
+  OnDestroy,
+  OnInit,
+} from "@angular/core";
 import { FormControl, Validators } from "@angular/forms";
 import { NgOtpInputConfig } from "ng-otp-input";
 import { CoreService } from "src/app/providers/core.service";
@@ -7,8 +14,9 @@ import { CoreService } from "src/app/providers/core.service";
   selector: "app-verify-otp",
   templateUrl: "./verify-otp.page.html",
   styleUrls: ["./verify-otp.page.scss"],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class VerifyOTPPage implements OnInit {
+export class VerifyOTPPage implements OnInit, OnDestroy {
   otpFormControl: FormControl = new FormControl<number | null>(null, [
     Validators.required,
     Validators.minLength(6),
@@ -23,7 +31,11 @@ export class VerifyOTPPage implements OnInit {
   interval: any;
   timeLeft: number = 120;
 
-  constructor(private coreService: CoreService) {}
+  constructor(
+    private coreService: CoreService,
+    private zone: NgZone,
+    private cd: ChangeDetectorRef
+  ) {}
 
   ngOnInit() {
     this.startTimer();
@@ -33,6 +45,7 @@ export class VerifyOTPPage implements OnInit {
     this.interval = setInterval(() => {
       if (this.timeLeft > 0) {
         this.timeLeft--;
+        this.cd.detectChanges();
       }
     }, 1000);
   }
@@ -60,5 +73,11 @@ export class VerifyOTPPage implements OnInit {
         this.coreService.TOAST_ERROR
       );
     }
+  }
+  ngOnDestroy(): void {
+    clearInterval(this.interval);
+  }
+  ionViewDidLeave(): void {
+    clearInterval(this.interval);
   }
 }
