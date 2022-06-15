3a553c78318639711f0c39add4e88c8fffc9acd6
nensi kachhadiya <nensi.k@techroversolutions.com>
2022-06-15 12:20:45 +0530

    [AK][BUBAPP-68]forgot password formgroup,validation and test case of forgot password

--- src/app/pages/auth-module/forgot-password/forgot-password.page.ts
+++ src/app/pages/auth-module/forgot-password/forgot-password.page.ts
@@ -1,15 +1,27 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit } from "@angular/core";
+import { FormControl, FormGroup, Validators } from "@angular/forms";
+import { Router } from "@angular/router";
+import { EMAIL_PATTERN } from "src/app/helpers/emailValidation";
 @Component({
-  selector: 'app-forgot-password',
-  templateUrl: './forgot-password.page.html',
-  styleUrls: ['./forgot-password.page.scss'],
+  selector: "app-forgot-password",
+  templateUrl: "./forgot-password.page.html",
+  styleUrls: ["./forgot-password.page.scss"],
 })
 export class ForgotPasswordPage implements OnInit {
+  emailController: FormControl = new FormControl<string | null>(null, [
+    Validators.required,
+    Validators.pattern(EMAIL_PATTERN),
+    Validators.email
+  ]);
+  isFormSubmitted = false;
+  constructor(private router: Router) {}
 
-  constructor() { }
-
-  ngOnInit() {
+  ngOnInit() {}
+  onSubmit() {
+    this.isFormSubmitted = true;
+    if (this.emailController.valid) {
+      this.router.navigate(["auth/verify-otp"]);
+    }
+    
   }
-
 }
