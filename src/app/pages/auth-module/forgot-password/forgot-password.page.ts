0e5117b454d64e924144b1b60cac980d8351c4c6
nensi kachhadiya <nensi.k@techroversolutions.com>
2022-06-15 18:10:23 +0530

    [AK][BUBAPP-68] email validation bug fix

--- src/app/pages/auth-module/forgot-password/forgot-password.page.ts
+++ src/app/pages/auth-module/forgot-password/forgot-password.page.ts
@@ -10,14 +10,14 @@ import { EMAIL_PATTERN } from "src/app/helpers/emailValidation";
 export class ForgotPasswordPage implements OnInit {
   emailController: FormControl = new FormControl<string | null>(null, [
     Validators.required,
-    Validators.pattern(EMAIL_PATTERN),
-    Validators.email
-  ]);
+    Validators.pattern("^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$")]);
+    
   isFormSubmitted = false;
   constructor(private router: Router) {}
 
   ngOnInit() {}
   onSubmit() {
+    console.log(this.emailController.errors)
     this.isFormSubmitted = true;
     if (this.emailController.valid) {
       this.router.navigate(["auth/verify-otp"]);
