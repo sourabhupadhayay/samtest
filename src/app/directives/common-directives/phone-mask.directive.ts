f89d1e2f61e18a0a49c6ee37b7d621c527526cc4
nensi kachhadiya <nensi.k@techroversolutions.com>
2022-06-16 14:35:19 +0530

    [AK][BUBAPP-44]phone directive issue solved

--- src/app/directives/common-directives/phone-mask.directive.ts
+++ src/app/directives/common-directives/phone-mask.directive.ts
@@ -1,46 +1,47 @@
-import { Directive, HostListener, Injector } from "@angular/core";
+import { Directive, HostListener } from "@angular/core";
 import { NgControl } from "@angular/forms";
+
 @Directive({
-  selector: "[formControlName][appPhoneMask]",
+  selector: "[formControlName][appPhoneMask]"
 })
 export class PhoneMaskDirective {
-  constructor(public ngControl: NgControl, private _injector: Injector) {}
-  @HostListener("change", ["$event"])
-  onchange(event: any) {
-    this.onInputChange(event.target.value, true);
-  }
-  @HostListener("focus", ["$event"])
-  onFocuschange(event: any) {
-    this.onInputChange(event.target.value, true);
-  }
+  constructor(public ngControl: NgControl) {}
+
   @HostListener("ngModelChange", ["$event"])
-  onModelChange(event: any) {
+  onModelChange(event) {
     this.onInputChange(event, false);
   }
+
   @HostListener("keydown.backspace", ["$event"])
-  keydownBackspace(event: any) {
+  keydownBackspace(event) {
     this.onInputChange(event.target.value, true);
   }
-  onInputChange(event: string, backspace: any) {
-    if (event) {
-      let newVal = event.replace(/\D/g, "");
 
-      if (backspace && newVal.length <= 6) {
-        newVal = newVal.substring(0, newVal.length - 1);
-      }
-      if (newVal.length === 0) {
-        newVal = "";
-      } else if (newVal.length <= 3) {
-        newVal = newVal.replace(/^(\d{0,3})/, "($1)");
-      } else if (newVal.length <= 6) {
-        newVal = newVal.replace(/^(\d{0,3})(\d{0,3})/, "($1) $2");
-      } else if (newVal.length <= 10) {
-        newVal = newVal.replace(/^(\d{0,3})(\d{0,3})(\d{0,4})/, "($1) $2-$3");
-      } else {
-        newVal = newVal.substring(0, 10);
-        newVal = newVal.replace(/^(\d{0,3})(\d{0,3})(\d{0,4})/, "($1) $2-$3");
-      }
-      this.ngControl.valueAccessor!.writeValue(newVal);
+  onInputChange(event, backspace) {
+    console.log("event - ", event);
+    if (event == null) {
+      return "";
+    }
+    let newVal = event.replace(/\D/g, "");
+    if (backspace && newVal.length <= 6) {
+      newVal = newVal.substring(0, newVal.length - 1);
+    }
+    if (newVal.length === 0) {
+      newVal = "";
+    } else if (newVal.length <= 3) {
+      newVal = newVal.replace(/^(\d{0,3})/, "($1)");
+    } else if (newVal.length <= 6) {
+      newVal = newVal.replace(/^(\d{0,3})(\d{0,3})/, "($1) $2");
+    } else {
+      newVal = newVal.replace(/^(\d{0,3})(\d{0,3})(\d{0,4})/, "($1) $2-$3");
+    }
+
+    console.log(newVal);
+
+    if (newVal === "") {
+      this.ngControl.valueAccessor?.writeValue(null);
+    } else {
+      this.ngControl.valueAccessor?.writeValue(newVal);
     }
   }
 }
