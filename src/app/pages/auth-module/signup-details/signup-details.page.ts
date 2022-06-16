9f38b302b16c48af7862b884d3d32289d778b55a
Kalph upadhyay <kalpupadhyay@gmail.com>
2022-06-16 17:09:45 +0530

    fixed merge conflict

--- src/app/pages/auth-module/signup-details/signup-details.page.ts
+++ src/app/pages/auth-module/signup-details/signup-details.page.ts
@@ -1,15 +1,51 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from "@angular/forms";
+import { format, parseISO, getDate, getMonth, getYear } from "date-fns";
 
 @Component({
-  selector: 'app-signup-details',
-  templateUrl: './signup-details.page.html',
-  styleUrls: ['./signup-details.page.scss'],
+  selector: "app-signup-details",
+  templateUrl: "./signup-details.page.html",
+  styleUrls: ["./signup-details.page.scss"],
 })
 export class SignupDetailsPage implements OnInit {
+  signUpDetailsForm: FormGroup;
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder) {}
 
   ngOnInit() {
+    this.initForm();
   }
 
+  initForm() {
+    this.signUpDetailsForm = this.formBuilder.group({
+      fullName: [null, [Validators.required]],
+      email: [null, [Validators.required]],
+      password: [null, Validators.required],
+      birthDate: [null, Validators.required],
+      phone: [null],
+      teamName: [null],
+      teamState: [null],
+      school: [null],
+    });
+  }
+
+  formatDate(value: string) {
+    return format(parseISO(value), "MM/dd/yyyy");
+  }
+
+  patchDateValue(date: string) {
+    let formattedDate = this.formatDate(date);
+    this.signUpDetailsForm.controls.birthDate.patchValue(formattedDate);
+
+    console.log(new Date(formattedDate));
+  }
+
+  onSubmit() {
+    console.log(this.signUpDetailsForm);
+  }
 }
