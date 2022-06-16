64c38ceb7d7da1c1e7841a09075238a1854be977
nensi kachhadiya <nensi.k@techroversolutions.com>
2022-06-16 15:12:52 +0530

    [AK][BUBAPP-44]phone directive issue solved

--- src/app/directives/common-directives/phone-mask.directive.ts
+++ src/app/directives/common-directives/phone-mask.directive.ts
@@ -2,7 +2,7 @@ import { Directive, HostListener } from "@angular/core";
 import { NgControl } from "@angular/forms";
 
 @Directive({
-  selector: "[formControlName][appPhoneMask]"
+  selector: "[formControlName][appPhoneMask]",
 })
 export class PhoneMaskDirective {
   constructor(public ngControl: NgControl) {}
@@ -18,10 +18,6 @@ export class PhoneMaskDirective {
   }
 
   onInputChange(event, backspace) {
-    console.log("event - ", event);
-    if (event == null) {
-      return "";
-    }
     let newVal = event.replace(/\D/g, "");
     if (backspace && newVal.length <= 6) {
       newVal = newVal.substring(0, newVal.length - 1);
@@ -29,19 +25,15 @@ export class PhoneMaskDirective {
     if (newVal.length === 0) {
       newVal = "";
     } else if (newVal.length <= 3) {
-      newVal = newVal.replace(/^(\d{0,3})/, "($1)");
+      newVal = newVal.replace(/^(\d{0,3})/, "$1");
     } else if (newVal.length <= 6) {
       newVal = newVal.replace(/^(\d{0,3})(\d{0,3})/, "($1) $2");
+    } else if (newVal.length <= 10) {
+      newVal = newVal.replace(/^(\d{0,3})(\d{0,3})(\d{0,4})/, "($1) $2-$3");
     } else {
+      newVal = newVal.substring(0, 10);
       newVal = newVal.replace(/^(\d{0,3})(\d{0,3})(\d{0,4})/, "($1) $2-$3");
     }
-
-    console.log(newVal);
-
-    if (newVal === "") {
-      this.ngControl.valueAccessor?.writeValue(null);
-    } else {
-      this.ngControl.valueAccessor?.writeValue(newVal);
-    }
+    this.ngControl.valueAccessor?.writeValue(newVal);
   }
 }
