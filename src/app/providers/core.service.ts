6fed1f1faba42fef647712417630254b8af1492f
Kalph upadhyay <kalpupadhyay@gmail.com>
2022-06-16 18:16:21 +0530

    uploading image

--- src/app/providers/core.service.ts
+++ src/app/providers/core.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from "@angular/core";
-
+import { Camera, CameraResultType, CameraSource } from "@capacitor/camera";
 import {
   ActionSheetController,
   AlertController,
@@ -50,7 +50,7 @@ export class CoreService {
       toast = this.toastCtrl.create(toastOpts);
     return (await toast).present();
   }
-  async presentLoader(): Promise<void> {
+  async presentLoader(message: string): Promise<void> {
     // Return;
     const loaderOpts: LoadingOptions = {
       spinner: "crescent",
@@ -61,6 +61,7 @@ export class CoreService {
       keyboardClose: true,
       showBackdrop: true,
       backdropDismiss: false,
+      message,
     };
     this.loading = this.loadingController?.create(loaderOpts);
     (await this.loading)?.onDidDismiss().then((): any => (this.loading = null));
@@ -103,4 +104,156 @@ export class CoreService {
     position = "relative";
     return { icon, position, color };
   }
+
+  async changeProfile() {
+    const actionSheet: HTMLIonActionSheetElement = await this.actionSheetCtrl.create(
+      {
+        header: "Select Option",
+        buttons: [
+          {
+            text: "Load from Library",
+            handler: (): void => {
+              this.pickImage();
+            },
+          },
+          {
+            text: "Use Camera",
+            handler: (): void => {
+              this.captureImage();
+            },
+          },
+        ],
+      }
+    );
+    await actionSheet.present();
+  }
+  b6toBlob(b64Data: any): Blob {
+    // Split the base64 string in data and contentType
+    const block: any = b64Data.split(";");
+    // Get the content type of the image
+    let contentType: any = block[0].split(":")[1]; // In this case "image/gif"
+    // Get the real base64 content of the file
+    const realData: any = block[1].split(",")[1]; //
+    contentType = contentType || "";
+    const sliceSize = 512;
+    const byteCharacters: string = atob(realData);
+    const byteArrays: any[] = [];
+    for (let offset = 0; offset < byteCharacters.length; offset += sliceSize) {
+      const slice: string = byteCharacters.slice(offset, offset + sliceSize);
+      const byteNumbers: any[] = new Array(slice.length);
+      for (let i = 0; i < slice.length; i++) {
+        byteNumbers[i] = slice.charCodeAt(i);
+      }
+      const byteArray: Uint8Array = new Uint8Array(byteNumbers);
+      byteArrays.push(byteArray);
+    }
+    const blob: Blob = new Blob(byteArrays, { type: contentType });
+    return blob;
+  }
+  uploadImage() {
+    return this.presentActionSheet();
+  }
+  async presentActionSheet() {
+    const actionSheet = await this.actionSheetCtrl.create({
+      header: "Select Option",
+      buttons: [
+        {
+          text: "Gallery",
+          handler: () => {
+            this.pickImage();
+          },
+        },
+        {
+          text: "Camera",
+          handler: () => {
+            this.captureImage();
+          },
+        },
+      ],
+    });
+    await actionSheet.present();
+  }
+
+  async captureImage(): Promise<string> {
+    const image = await Camera.getPhoto({
+      quality: 90,
+      allowEditing: true,
+      resultType: CameraResultType.Uri,
+    });
+
+    // image.webPath will contain a path that can be set as an image src.
+    // You can access the original file using image.path, which can be
+    // passed to the Filesystem API to read the raw data of the image,
+    // if desired (or pass resultType: CameraResultType.Base64 to getPhoto)
+    // var imageUrl = image.webPath;
+
+    // Can be set to the src of an image now
+    // imageElement.src = imageUrl;
+    return image.webPath;
+  }
+
+  async pickImage() {
+    this.presentLoader(this.constant.UPLOADING);
+
+    const image = await Camera.getPhoto({
+      quality: 90,
+      allowEditing: true,
+      resultType: CameraResultType.Uri,
+      source: CameraSource.Prompt,
+    });
+
+    if (image) {
+    }
+
+    return image;
+    // this.camera.getPicture(options).then(
+    //   (imageData) => {
+    //     /*
+    //      * ImageData is either a base64 encoded string or a file URI
+    //      * If it's base64 (DATA_URL):
+    //      */
+
+    //     console.log(`==>>img ${imageData}`);
+    //     const img = `data:image/jpeg;base64,${imageData}`;
+    //     this.uploadImageToServer(img);
+    //     this.images = img;
+    //     console.log(`img ${imageData}`);
+    //   },
+    //   (err) => {
+    //     this.dismissLoader();
+    //     console.log(err);
+    //   }
+    // );
+  }
+
+  // uploadImageToServer(image) {
+  //   this.presentLoader(this.constant.UPLOADING);
+  //   //const imageFile = this.b6toBlob(image);
+  //   // const params = new FormData();
+  //   //params.append("file", imageFile, `${Date.parse(new Date().toDateString())}.jpg`);
+  //   //console.log(params);
+  //   //let image: string = this.EXAMPLEIMG;
+  //   const imageFile: Blob = this.b6toBlob(image);
+  //   const params: FormData = new FormData();
+  //   params.append("file", imageFile);
+  //   console.log(params);
+  //   return this.dataService
+  //     .imgUpload(this.constant.PROFILE_PICTURE_UPLOAD, params)
+  //     .subscribe((response: any) => {
+  //       if (response.status.code == this.constant.STATUS_OK) {
+  //         this.common.profileUrl = response.data;
+  //         this.dismissLoader();
+  //         this.showToastMessage(
+  //           response.status.description,
+  //           this.constant.TOAST_SUCCESS
+  //         );
+  //       } else {
+  //         this.dismissLoader();
+  //         this.constant.showToastMessage(
+  //           response.status.description,
+  //           this.constant.TOAST_ERROR
+  //         );
+  //       }
+  //     });
+  // }
 }
