e38d319711361e8f80c58a32bfb8605f8339d2a7
Kalph upadhyay <kalpupadhyay@gmail.com>
2022-06-22 10:12:54 +0530

    integrated forgot email api

--- src/app/pages/auth-module/forgot-password/forgot-password.page.ts
+++ src/app/pages/auth-module/forgot-password/forgot-password.page.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from "@angular/core";
 import { FormControl, FormGroup, Validators } from "@angular/forms";
 import { Router } from "@angular/router";
 import { EMAIL_PATTERN } from "src/app/helpers/emailValidation";
+import { ConstantService } from "src/app/providers/constant.service";
+import { CoreService } from "src/app/providers/core.service";
+import { DataService, Request, Response } from "src/app/providers/data.service";
 @Component({
   selector: "app-forgot-password",
   templateUrl: "./forgot-password.page.html",
@@ -10,18 +13,45 @@ import { EMAIL_PATTERN } from "src/app/helpers/emailValidation";
 export class ForgotPasswordPage implements OnInit {
   emailController: FormControl = new FormControl<string | null>(null, [
     Validators.required,
-    Validators.pattern("^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$")]);
-    
+    Validators.pattern("^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$"),
+  ]);
+
   isFormSubmitted = false;
-  constructor(private router: Router) {}
+  constructor(
+    private coreService: CoreService,
+    private apiService: DataService,
+    private constantService: ConstantService,
+    private router: Router
+  ) {}
 
   ngOnInit() {}
   onSubmit() {
-    console.log(this.emailController.errors)
     this.isFormSubmitted = true;
-    if (this.emailController.valid) {
-      this.router.navigate(["auth/verify-otp"]);
+    if (this.emailController.invalid) {
+      return;
     }
-    
+
+    let request: Request = {
+      path: "auth/users/user/password/forget",
+      data: {
+        email: this.emailController.value,
+      },
+    };
+    this.coreService.presentLoader(this.constantService.WAIT);
+    this.apiService.post(request).subscribe((response: Response) => {
+      this.coreService.dismissLoader();
+      if (response["status"]["code"] === "OK") {
+        this.router.navigate(["auth/verify-otp"]);
+        this.coreService.showToastMessage(
+          response.status.description,
+          this.coreService.TOAST_SUCCESS
+        );
+      } else {
+        this.coreService.showToastMessage(
+          response.status.description,
+          this.coreService.TOAST_ERROR
+        );
+      }
+    });
   }
 }
