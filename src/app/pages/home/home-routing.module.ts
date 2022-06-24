5cfeef60eb6c02e271907e5c86b88f72f5e05977
Kalph upadhyay <kalpupadhyay@gmail.com>
2022-06-24 15:40:00 +0530

    fixed conflict

--- src/app/pages/home/home-routing.module.ts
+++ src/app/pages/home/home-routing.module.ts
@@ -1,32 +1,34 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
-<<<<<<< HEAD
+
 import { AuthGuard } from "src/app/guard/auth.guard";
 
 const routes: Routes = [
   {
-    path: "profile",
-    canActivate: [AuthGuard],
+    path: "home",
     children: [
       {
-        path: "athlete",
-        loadChildren: () =>
-          import("./profiles/athlete/athlete.module").then(
-            (m) => m.AthletePageModule
-          ),
-      },
-      {
-        path: "user",
-        loadChildren: () =>
-          import("./profiles/user/user.module").then((m) => m.UserPageModule),
+        path: "profile",
+        children: [
+          {
+            path: "athlete",
+            loadChildren: () =>
+              import("./profiles/athlete/athlete.module").then(
+                (m) => m.AthletePageModule
+              ),
+          },
+          {
+            path: "user",
+            loadChildren: () =>
+              import("./profiles/user/user.module").then(
+                (m) => m.UserPageModule
+              ),
+          },
+        ],
       },
     ],
   },
 ];
-=======
-
-const routes: Routes = [];
->>>>>>> 5790f4d (code refactoring)
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
