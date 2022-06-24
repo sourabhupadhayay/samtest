fd7323b1090d1c2d62599b9d595e292ae91d0491
Kalph upadhyay <kalpupadhyay@gmail.com>
2022-06-24 12:28:56 +0530

    code refactoring

--- src/app/pages/home/home-routing.module.ts
+++ src/app/pages/home/home-routing.module.ts
@@ -1,7 +1,32 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "home",
+    children: [
+      {
+        path: "profile",
+        children: [
+          {
+            path: "athlete",
+            loadChildren: () =>
+              import("./profiles/athlete/athlete.module").then(
+                (m) => m.AthletePageModule
+              ),
+          },
+          {
+            path: "user",
+            loadChildren: () =>
+              import("./profiles/user/user.module").then(
+                (m) => m.UserPageModule
+              ),
+          },
+        ],
+      },
+    ],
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
---
+++ src/app/pages/home/profiles/athlete/athlete.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-athlete',
+  templateUrl: './athlete.page.html',
+  styleUrls: ['./athlete.page.scss'],
+})
+export class AthletePage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
