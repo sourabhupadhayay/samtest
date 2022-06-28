ca58b2212928b5dc9ab697c63666e15ca3c229a6
Kalph upadhyay <kalpupadhyay@gmail.com>
2022-06-28 14:41:25 +0530

    integrated api

--- src/app/pages/home/profiles/athlete/view-profile/view-profile.page.ts
+++ src/app/pages/home/profiles/athlete/view-profile/view-profile.page.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute, Router } from "@angular/router";
 import { ModalController } from "@ionic/angular";
+import { ConstantService } from "src/app/providers/constant.service";
+import { CoreService } from "src/app/providers/core.service";
+import { DataService, Request, Response } from "src/app/providers/data.service";
 
 @Component({
   selector: "app-view-profile",
@@ -7,11 +11,42 @@ import { ModalController } from "@ionic/angular";
   styleUrls: ["./view-profile.page.scss"],
 })
 export class ViewProfilePage implements OnInit {
-  constructor(public modalCtrl: ModalController) {}
+  athleteData: any | null = null;
 
-  ngOnInit() {}
+  constructor(
+    public modalCtrl: ModalController,
+    private coreService: CoreService,
+    private apiService: DataService,
+    private constantService: ConstantService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) {}
+
+  ngOnInit() {
+    this.getCurrentUserDetails();
+  }
 
   onclick_cancel(): void {
     this.modalCtrl.dismiss();
+  }
+
+  getCurrentUserDetails() {
+    let request: Request = {
+      path: "auth/users/currentUser",
+      isAuth: true,
+    };
+    this.coreService.presentLoader(this.constantService.WAIT);
+
+    this.apiService.get(request).subscribe((response: Response) => {
+      this.coreService.dismissLoader();
+      if (response.status.code === this.constantService.STATUS_OK) {
+        this.athleteData = response.data;
+      } else {
+        this.coreService.showToastMessage(
+          response.status.description,
+          this.coreService.TOAST_ERROR
+        );
+      }
+    });
   }
 }
