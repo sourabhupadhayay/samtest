32d3646b594997f066e22529b66a9360e5e56120
Kalph upadhyay <kalpupadhyay@gmail.com>
2022-07-04 12:26:04 +0530

    loader issue fix

--- src/app/pages/home/profile/view-profile/view-profile.page.ts
+++ src/app/pages/home/profile/view-profile/view-profile.page.ts
@@ -66,21 +66,21 @@ export class ViewProfilePage implements OnInit {
       path: "auth/users/currentUser",
       isAuth: true,
     };
-    this.coreService.presentLoader(this.constantService.WAIT);
-
-    this.apiService.get(request).subscribe((response: Response) => {
-      this.coreService.dismissLoader().then(() => {
-        if (response.status.code === this.constantService.STATUS_OK) {
-          this.userData = response.data;
-          this.nameInitials = this.commonService.getInitials(
-            this.userData.fullName
-          );
-        } else {
-          this.coreService.showToastMessage(
-            response.status.description,
-            this.coreService.TOAST_ERROR
-          );
-        }
+    this.coreService.presentLoader(this.constantService.WAIT).then(() => {
+      this.apiService.get(request).subscribe((response: Response) => {
+        this.coreService.dismissLoader().then(() => {
+          if (response.status.code === this.constantService.STATUS_OK) {
+            this.userData = response.data;
+            this.nameInitials = this.commonService.getInitials(
+              this.userData.fullName
+            );
+          } else {
+            this.coreService.showToastMessage(
+              response.status.description,
+              this.coreService.TOAST_ERROR
+            );
+          }
+        });
       });
     });
   }
