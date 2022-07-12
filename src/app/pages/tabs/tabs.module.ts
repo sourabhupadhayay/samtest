a7001c24259bc6363aabd1ac277e91e7074d542b
Kalph upadhyay <kalpupadhyay@gmail.com>
2022-07-12 18:25:54 +0530

    added hour and min

--- src/app/pages/tabs/tabs.module.ts
+++ src/app/pages/tabs/tabs.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
-import { FormsModule } from "@angular/forms";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 
 import { IonicModule } from "@ionic/angular";
 
@@ -17,6 +17,7 @@ import { AppereanceBookingComponent } from "./appereance-booking/appereance-book
     IonicModule,
     TabsPageRoutingModule,
     PipesModule,
+    ReactiveFormsModule,
   ],
   declarations: [TabsPage, AppereanceBookingComponent],
 })
