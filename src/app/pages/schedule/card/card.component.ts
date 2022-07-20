af928ab94a357d9b743804aaa5ddac8f767c0be2
foram <foram.s@techroversolutions.com>
2022-07-20 10:28:08 +0530

    [FS]-timer issue

--- src/app/pages/schedule/card/card.component.ts
+++ src/app/pages/schedule/card/card.component.ts
@@ -7,73 +7,48 @@ import { Component, Input, OnInit } from "@angular/core";
 })
 export class CardComponent implements OnInit {
   @Input() cardData;
+  liveTime:any;
+  counter:any;
+  timer:any;
   constructor() {}
 
   ngOnInit() {
-    // this.dateFormat();
+    console.log(this.cardData.startDate);
+    this.dateFormat();
+  }
+  dateFormat() {
+      var countDownDate = new Date(this.cardData.startDate).getTime();
+      let days
+      let hours
+      let minutes
+      let seconds
+      let now
+      let timeleft
+      let counter
+      var myfunc= setInterval(()=> {
+        now = new Date().getTime();
+        timeleft = countDownDate - now;
+        days = Math.floor(timeleft / (1000 * 60 * 60 * 24));
+        hours = Math.floor((timeleft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        minutes = Math.floor((timeleft % (1000 * 60 * 60)) / (1000 * 60));
+        seconds = Math.floor((timeleft % (1000 * 60)) / 1000);
+        this.liveTime = days +'days' +' : ' + hours+ 'hours'+' : '+ minutes + 'mins '+' : ' + seconds + 'S'
+        if(days>=1){
+          this.counter={"days": days,"title_days" :'days', "hours": hours,"title_hours" :'hours',"minutes":minutes,"title_min":'mins',"sec": seconds,"title_sec" :'S'}
+        }
+        else{
+          if(days<1 && hours<1){
+            this.counter={"minutes":minutes,"title_min":'mins',"sec": seconds,"title_sec" :'S'}
+          }
+          else{
+            this.counter={ "hours": hours,"title_hours" :'hours',"minutes":minutes,"title_min":'mins',"sec": seconds,"title_sec" :'S'}
+          }
+
+        }
+        this.timer =this.counter;
+        console.log("dsvfdg",this.timer)
+
+      },1000)
   }
 
-  //   dateFormat() {
-  //     var countDownDate = new Date(this.cardData.createdDate).getTime();
-  //     let days;
-  //     let hours;
-  //     let minutes;
-  //     let seconds;
-  //     let now;
-  //     let timeleft;
-  //     let counter;
-  //     var myfunc = setInterval(() => {
-  //       now = new Date().getTime();
-  //       timeleft = countDownDate - now;
-  //       days = Math.floor(timeleft / (1000 * 60 * 60 * 24));
-  //       hours = Math.floor((timeleft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  //       minutes = Math.floor((timeleft % (1000 * 60 * 60)) / (1000 * 60));
-  //       seconds = Math.floor((timeleft % (1000 * 60)) / 1000);
-  //       var liveTime =
-  //         days +
-  //         "DAYS" +
-  //         " : " +
-  //         hours +
-  //         "H" +
-  //         " : " +
-  //         minutes +
-  //         "M " +
-  //         " : " +
-  //         seconds +
-  //         "S";
-  //       if (days >= 1) {
-  //         counter = [
-  //           {
-  //             days: days,
-  //             title_days: "DAY",
-  //             hours: hours,
-  //             title_hours: "H",
-  //             minutes: minutes,
-  //             title_min: "M",
-  //             sec: seconds,
-  //             title_sec: "S",
-  //           },
-  //         ];
-  //       } else {
-  //         if (days < 1 && hours < 1) {
-  //           counter = [
-  //             { minutes: minutes, title_min: "M", sec: seconds, title_sec: "S" },
-  //           ];
-  //         } else {
-  //           counter = [
-  //             {
-  //               hours: hours,
-  //               title_hours: "H",
-  //               minutes: minutes,
-  //               title_min: "M",
-  //               sec: seconds,
-  //               title_sec: "S",
-  //             },
-  //           ];
-  //         }
-  //       }
-
-  //       console.log(liveTime);
-  //     }, 1000);
-  //   }
 }
