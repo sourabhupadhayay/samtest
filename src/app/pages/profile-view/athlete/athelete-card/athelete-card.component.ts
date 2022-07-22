05f3c323acdd09193590051552d976e42f5a08bf
Kalph upadhyay <kalpupadhyay@gmail.com>
2022-07-22 12:18:27 +0530

    created athlete card

---
+++ src/app/pages/profile-view/athlete/athelete-card/athelete-card.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-athelete-card',
+  templateUrl: './athelete-card.component.html',
+  styleUrls: ['./athelete-card.component.scss'],
+})
+export class AtheleteCardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
