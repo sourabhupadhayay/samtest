64127e5bd87e429d2cf723c03936a8f82baf5c3a
Hardik Adrola <hardik.a@techroversolutions.com>
2022-08-01 19:22:17 +0530

    [HA][Bubapp-257] Account detais screen design

--- src/app/pages/invoice/invoice.page.ts
+++ src/app/pages/invoice/invoice.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { IonModal, ModalController } from "@ionic/angular";
 
 @Component({
   selector: 'app-invoice',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InvoicePage implements OnInit {
 
-  constructor() { }
+  constructor(public modalCtrl: ModalController) { }
 
   ngOnInit() {
+  }
+
+  onClickCancel() {
+    this.modalCtrl.dismiss();
   }
 
 }
