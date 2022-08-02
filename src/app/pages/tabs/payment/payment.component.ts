ebbd54f1510b84cd1ed2311bcd98b673bb302e85
foram <foram.s@techroversolutions.com>
2022-08-02 09:52:20 +0530

    [FS]square payment integration

--- src/app/pages/tabs/payment/payment.component.ts
+++ src/app/pages/tabs/payment/payment.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, ViewChild } from "@angular/core";
 import { IonModal, ModalController } from "@ionic/angular";
-declare var SqPaymentForm: any; //magic to allow us to access the SquarePaymentForm lib
+declare var SqPaymentForm: any;
+//magic to allow us to access the SquarePaymentForm lib
 @Component({
   selector: "app-payment",
   templateUrl: "./payment.component.html",
@@ -10,6 +11,7 @@ export class PaymentComponent implements OnInit {
   @ViewChild(IonModal) modal: IonModal;
   paymentForm; //this is our payment form object
   constructor(public modalCtrl: ModalController) {}
+  sqPaymentForm: any;
 
   onClickCancel() {
     this.modalCtrl.dismiss();
@@ -24,173 +26,62 @@ export class PaymentComponent implements OnInit {
 
   ngOnInit() {
     var applicationId = "sandbox-sq0idb-r92A0oxpvi6g0Y1wHRzYwA";
-    this.paymentForm = new SqPaymentForm({
+    this.sqPaymentForm = new SqPaymentForm({
       // Initialize the payment form elements
+
+      //TODO: Replace with your sandbox application ID
       applicationId: applicationId,
-      inputClass: "sq-input",
-
+      inputClass: 'sq-input',
+      autoBuild: false,
       // Customize the CSS for SqPaymentForm iframe elements
-      inputStyles: [
-        {
-          fontSize: ".9em",
-        },
-      ],
-
+      inputStyles: [{
+        fontSize: '16px',
+        lineHeight: '24px',
+        padding: '16px',
+        placeholderColor: '#a0a0a0',
+        backgroundColor: 'transparent',
+      }],
       // Initialize the credit card placeholders
       cardNumber: {
-        elementId: "sq-card-number",
-        placeholder: "•••• •••• •••• ••••",
+        elementId: 'sq-card-number',
+        placeholder: 'Card Number'
       },
       cvv: {
-        elementId: "sq-cvv",
-        placeholder: "CVV",
+        elementId: 'sq-cvv',
+        placeholder: 'CVV'
       },
       expirationDate: {
-        elementId: "sq-expiration-date",
-        placeholder: "MM/YY",
+        elementId: 'sq-expiration-date',
+        placeholder: 'MM/YY'
       },
       postalCode: {
-        elementId: "sq-postal-code",
+        elementId: 'sq-postal-code',
+        placeholder: 'Postal'
       },
-
       // SqPaymentForm callback functions
       callbacks: {
         /*
-         * callback function: methodsSupported
-         * Triggered when: the page is loaded.
-         */
-        methodsSupported: function (methods) {
-          var applePayBtn = document.getElementById("sq-apple-pay");
-          var applePayLabel = document.getElementById("sq-apple-pay-label");
-          var masterpassBtn = document.getElementById("sq-masterpass");
-          var masterpassLabel = document.getElementById("sq-masterpass-label");
-
-          // Only show the button if Apple Pay for Web is enabled
-          // Otherwise, display the wallet not enabled message.
-          if (methods.applePay === true) {
-            applePayBtn.style.display = "inline-block";
-            applePayLabel.style.display = "none";
-          }
-          // Only show the button if Masterpass is enabled
-          // Otherwise, display the wallet not enabled message.
-          if (methods.masterpass === true) {
-            masterpassBtn.style.display = "inline-block";
-            masterpassLabel.style.display = "none";
-          }
-        },
-
-        /*
-         * callback function: createPaymentRequest
-         * Triggered when: a digital wallet payment button is clicked.
-         */
-        createPaymentRequest: function () {
-          // The payment request below is provided as
-          // guidance. You should add code to create the object
-          // programmatically.
-          return {
-            requestShippingAddress: true,
-            currencyCode: "USD",
-            countryCode: "US",
-            total: {
-              label: "Hakuna",
-              amount: "{{REPLACE_ME}}",
-              pending: false,
-            },
-            lineItems: [
-              {
-                label: "Subtotal",
-                amount: "{{REPLACE_ME}}",
-                pending: false,
-              },
-              {
-                label: "Shipping",
-                amount: "{{REPLACE_ME}}",
-                pending: true,
-              },
-              {
-                label: "Tax",
-                amount: "{{REPLACE_ME}}",
-                pending: false,
-              },
-            ],
-          };
-        },
-
-        /*
-         * callback function: cardNonceResponseReceived
-         * Triggered when: SqPaymentForm completes a card nonce request
-         */
+        * callback function: cardNonceResponseReceived
+        * Triggered when: SqPaymentForm completes a card nonce request
+        */
         cardNonceResponseReceived: function (errors, nonce, cardData) {
           if (errors) {
-            // Log errors from nonce generation to the Javascript console
-            console.log("Encountered errors:");
+            // Log errors from nonce generation to the browser developer console.
+            console.error('Encountered errors:');
             errors.forEach(function (error) {
-              console.log("  " + error.message);
+              console.error('  ' + error.message);
             });
-
+            alert('Encountered errors, check browser developer console for more details');
             return;
           }
-
-          alert("Nonce received: " + nonce); /* FOR TESTING ONLY */
-
-          // Assign the nonce value to the hidden form field
-          // document.getElementById('card-nonce').value = nonce;
-          //needs to be extracted from the
-          (<HTMLInputElement>(
-            document.getElementById("card-nonce")
-          )).value = nonce; //casting so .value will work
-          //get this value from the database when the user is logged in
-          (<HTMLInputElement>document.getElementById("sq-id")).value =
-            "CBASEC8F-Phq5_pV7UNi64_kX_4gAQ";
-
-          // POST the nonce form to the payment processing page
-          (<HTMLFormElement>document.getElementById("nonce-form")).submit();
-        },
-
-        /*
-         * callback function: unsupportedBrowserDetected
-         * Triggered when: the page loads and an unsupported browser is detected
-         */
-        unsupportedBrowserDetected: function () {
-          /* PROVIDE FEEDBACK TO SITE VISITORS */
-        },
-
-        /*
-         * callback function: inputEventReceived
-         * Triggered when: visitors interact with SqPaymentForm iframe elements.
-         */
-        inputEventReceived: function (inputEvent) {
-          switch (inputEvent.eventType) {
-            case "focusClassAdded":
-              /* HANDLE AS DESIRED */
-              break;
-            case "focusClassRemoved":
-              /* HANDLE AS DESIRED */
-              break;
-            case "errorClassAdded":
-              /* HANDLE AS DESIRED */
-              break;
-            case "errorClassRemoved":
-              /* HANDLE AS DESIRED */
-              break;
-            case "cardBrandChanged":
-              /* HANDLE AS DESIRED */
-              break;
-            case "postalCodeChanged":
-              /* HANDLE AS DESIRED */
-              break;
-          }
-        },
-
-        /*
-         * callback function: paymentFormLoaded
-         * Triggered when: SqPaymentForm is fully loaded
-         */
-        paymentFormLoaded: function () {
-          /* HANDLE AS DESIRED */
-        },
-      },
+          alert(`The generated nonce is:\n${nonce}`);
+          //TODO: Replace alert with code in step 2.1
+        }
+      }
     });
+    //TODO: paste code from step 1.1.4
+    this.sqPaymentForm.build();
+    console.log("foram",this.sqPaymentForm)
   }
   requestCardNonce(event) {
     // Don't submit the form until SqPaymentForm returns with a nonce
@@ -199,4 +90,5 @@ export class PaymentComponent implements OnInit {
     // Request a nonce from the SqPaymentForm object
     this.paymentForm.requestCardNonce();
   }
+
 }
