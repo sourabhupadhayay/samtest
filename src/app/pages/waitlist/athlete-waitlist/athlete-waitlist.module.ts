04e62fd73dbc71bd04e9fcf36b5c2b7516da8996
Kalph upadhyay <kalpupadhyay@gmail.com>
2022-08-05 10:09:46 +0530

    added socket package

--- src/app/pages/waitlist/athlete-waitlist/athlete-waitlist.module.ts
+++ src/app/pages/waitlist/athlete-waitlist/athlete-waitlist.module.ts
@@ -1,20 +1,20 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-
-import { IonicModule } from '@ionic/angular';
-
-import { AthleteWaitlistPageRoutingModule } from './athlete-waitlist-routing.module';
-
-import { AthleteWaitlistPage } from './athlete-waitlist.page';
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { FormsModule } from "@angular/forms";
+import { IonicModule } from "@ionic/angular";
+import { AthleteWaitlistPageRoutingModule } from "./athlete-waitlist-routing.module";
+import { AthleteWaitlistPage } from "./athlete-waitlist.page";
+import { SocketIoModule, SocketIoConfig } from "ngx-socket-io";
+const config: SocketIoConfig = { url: "http://localhost:3000", options: {} };
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    AthleteWaitlistPageRoutingModule
+    AthleteWaitlistPageRoutingModule,
+    SocketIoModule.forRoot(config),
   ],
-  declarations: [AthleteWaitlistPage]
+  declarations: [AthleteWaitlistPage],
 })
 export class AthleteWaitlistPageModule {}
