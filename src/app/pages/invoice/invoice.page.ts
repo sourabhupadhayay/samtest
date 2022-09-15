3cd7c82ee028fe00808c7a01ec77724461b9bc5a
Kalph upadhyay <kalpupadhyay@gmail.com>
2022-09-15 17:20:01 +0530

    added terms and condtions modal

--- src/app/pages/invoice/invoice.page.ts
+++ src/app/pages/invoice/invoice.page.ts
@@ -1,20 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 import { IonModal, ModalController } from "@ionic/angular";
+import { TermsOfServiceComponent } from "./terms-of-service/terms-of-service.component";
 
 @Component({
-  selector: 'app-invoice',
-  templateUrl: './invoice.page.html',
-  styleUrls: ['./invoice.page.scss'],
+  selector: "app-invoice",
+  templateUrl: "./invoice.page.html",
+  styleUrls: ["./invoice.page.scss"],
 })
 export class InvoicePage implements OnInit {
+  constructor(public modalCtrl: ModalController) {}
 
-  constructor(public modalCtrl: ModalController) { }
-
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
   onClickCancel() {
     this.modalCtrl.dismiss();
   }
 
+  async presentTermsAndServices() {
+    const modal: HTMLIonModalElement = await this.modalCtrl.create({
+      component: TermsOfServiceComponent,
+      cssClass: "tandc-modal",
+    });
+    modal.present();
+  }
 }
