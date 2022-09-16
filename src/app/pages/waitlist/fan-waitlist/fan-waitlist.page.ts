f705a7b1e27b5c2011db602571c257d160381744
Kalph upadhyay <kalpupadhyay@gmail.com>
2022-09-16 14:14:46 +0530

    text changes

--- src/app/pages/waitlist/fan-waitlist/fan-waitlist.page.ts
+++ src/app/pages/waitlist/fan-waitlist/fan-waitlist.page.ts
@@ -1,7 +1,9 @@
 import {
+  ChangeDetectorRef,
   Component,
   Input,
   OnChanges,
+  OnDestroy,
   OnInit,
   SimpleChanges,
 } from "@angular/core";
@@ -17,27 +19,30 @@ import { DataService, Request, Response } from "src/app/providers/data.service";
   templateUrl: "./fan-waitlist.page.html",
   styleUrls: ["./fan-waitlist.page.scss"],
 })
-export class FanWaitlistPage implements OnInit {
+export class FanWaitlistPage implements OnInit, OnDestroy {
   @Input() eventId: null | string = null;
   @Input() connectedFans: any[] = [];
   userData;
+  eventData;
   currentPosition: number = 0;
   maxBid: number;
   nameInitials: string;
-  // userBidDetails: null | any = null;
+  eventTime;
   userIndex: number = 0;
+  interval;
 
   constructor(
     private coreService: CoreService,
     private apiService: DataService,
     private router: Router,
     private constantService: ConstantService,
-    private commonService: CommonService
+    private commonService: CommonService,
+    private cd: ChangeDetectorRef
   ) {}
 
   ngOnInit() {
     this.getUserData();
-    // this.getMaximumBidForEvent();
+    this.getEventDetails();
   }
 
   ngDoCheck() {
@@ -55,26 +60,28 @@ export class FanWaitlistPage implements OnInit {
     }
   }
 
-  // getMaximumBidForEvent() {
-  //   let request: Request = {
-  //     path: "core/event/bid/max/" + this.eventId,
-  //     isAuth: true,
-  //   };
+  getEventDetails() {
+    let request: Request = {
+      path: "core/event/details/" + this.eventId,
+      isAuth: true,
+    };
 
-  //   this.coreService.presentLoader(this.constantService.WAIT);
-  //   this.apiService.get(request).subscribe((response: Response) => {
-  //     this.coreService.dismissLoader();
-  //     if (response.status.code === this.constantService.STATUS_OK) {
-  //       this.maxBid = response.data.maxBid.totalAmount;
-  //       this.userBidDetails = response.data.currentBid;
-  //     } else {
-  //       this.coreService.showToastMessage(
-  //         response.status.description,
-  //         this.coreService.TOAST_ERROR
-  //       );
-  //     }
-  //   });
-  // }
+    this.coreService.presentLoader(this.constantService.WAIT);
+    this.apiService.get(request).subscribe((response: Response) => {
+      this.coreService.dismissLoader();
+      if (response.status.code === this.constantService.STATUS_OK) {
+        this.eventData = response.data;
+        this.calculateTime();
+
+        this.cd.detectChanges();
+      } else {
+        this.coreService.showToastMessage(
+          response.status.description,
+          this.coreService.TOAST_ERROR
+        );
+      }
+    });
+  }
 
   async getUserData() {
     this.userData = await this.coreService.getUserDataFromStorage();
@@ -93,5 +100,16 @@ export class FanWaitlistPage implements OnInit {
       return 1;
     }
     return 0;
+  }
+
+  calculateTime() {
+    this.eventTime = this.commonService.dateFormat(this.eventData.startDate);
+    this.interval = setInterval(() => {
+      this.eventTime = this.commonService.dateFormat(this.eventData.startDate);
+    }, 60000);
+  }
+
+  ngOnDestroy(): void {
+    clearInterval(this.interval);
   }
 }
