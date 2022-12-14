1d36ec1839399275e6907d8be95d060678bb4819
bansi <bansi.v@techroversolutions.com>
2022-12-14 07:49:53 -0600

    [BV][BUBAPP-1.2]term & condition add apperance page

--- src/app/pages/tabs/terms-conditions/terms-conditions.component.ts
+++ src/app/pages/tabs/terms-conditions/terms-conditions.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from "@angular/core";
 import { ModalController } from "@ionic/angular";
+import { ConstantService } from "src/app/providers/constant.service";
+import { CoreService } from "src/app/providers/core.service";
+import { DataService,Request,Response } from "src/app/providers/data.service";
 
 @Component({
   selector: "app-terms-conditions",
@@ -7,11 +10,37 @@ import { ModalController } from "@ionic/angular";
   styleUrls: ["./terms-conditions.component.scss"],
 })
 export class TermsConditionsComponent implements OnInit {
-  constructor(public modalCtrl: ModalController) {}
-
-  ngOnInit() {}
-
+  constructor(public modalCtrl: ModalController, private coreService: CoreService,  private constantService: ConstantService,private apiService: DataService) {}
+  term:any;
+  ngOnInit() {
+    this.policy();
+  }
+ 
   onclick_cancel() {
     this.modalCtrl.dismiss();
+  }
+  policy(){
+    let request: Request = {
+      path: "auth/configuration/getTermsAndCondition",
+      
+    };
+
+    this.coreService.presentLoader(this.constantService.WAIT);
+
+    this.apiService.get(request).subscribe((response: Response) => {
+      this.coreService.dismissLoader();
+      if (response.status.code === this.constantService.STATUS_OK) {
+     
+       this.term=response.data.termsAndCondition;
+       console.log(this.term);
+     
+      } else {
+        this.coreService.showToastMessage(
+          response.status.description,
+          this.coreService.TOAST_ERROR
+        );
+      }
+    });
+    
   }
 }
