4e5b854a1fe361cf8fbdd497d67a4204e738f801
Alfaz Vahora <alfaz.v@techroversolutions.com>
2023-01-04 05:04:31 -0600

    [AV] [BUBAPP-748] FAQ's integrated

--- src/app/pages/help-section/help-section.page.ts
+++ src/app/pages/help-section/help-section.page.ts
@@ -1,7 +1,9 @@
 import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
-import { PopoverController } from '@ionic/angular';
+import { Preferences } from '@capacitor/preferences';
+import { ModalController, PopoverController } from '@ionic/angular';
 import { CommonService } from 'src/app/providers/common.service';
+import { ConstantService } from 'src/app/providers/constant.service';
 import { CoreService } from 'src/app/providers/core.service';
 import { DataService } from 'src/app/providers/data.service';
 import { PushNotificationPage } from '../push-notification/push-notification.page';
@@ -14,14 +16,35 @@ import { PushNotificationPage } from '../push-notification/push-notification.pag
 export class HelpSectionPage implements OnInit {
 
   badgeCount : number = 0;
+  feedbackMsg : string = '';
+  loggedInUserData : any;
+  currentUserRole : any;
+  isModalOpen : boolean = false;
+  faqList : any;
+
+
 
   constructor(
     private apiService: DataService,
     public popoverController: PopoverController,
+    public coreService : CoreService,
+    public constant : ConstantService,
+    public commonService : CommonService,
+    public modal : ModalController
   ) {
   }
 
   ngOnInit() {
+    this.getUserDataFromStorage();
+    this.getFAQList();
+  }
+
+  async getUserDataFromStorage() {
+    const { value } = await Preferences.get({ key: "userDetails" });
+    this.loggedInUserData = JSON.parse(value);
+    this.currentUserRole = this.commonService.getUserType(
+      this.loggedInUserData.roles
+    );
   }
 
   async presentPopover(ev: any) {
@@ -51,5 +74,55 @@ export class HelpSectionPage implements OnInit {
         return this.badgeCount;
       });
   }
+
+  submitFeedBack() {
+    if(this.feedbackMsg == '') {
+      this.coreService.showToastMessage(
+        "Please enter feedback!",
+        this.coreService.TOAST_ERROR
+      );
+      return;
+    } else {
+      let request: any = {
+        path: "core/contact/submit",
+        data: {
+          "email": this.loggedInUserData?.email,
+          "fullName": this.loggedInUserData?.fullName,
+          "message": this.feedbackMsg,
+          "role": this.currentUserRole
+        },
+        isAuth: false,
+      };
+      this.coreService.presentLoader(this.constant.WAIT);
+      this.apiService.post(request).subscribe((response: any) => {
+        this.coreService.dismissLoader();
+        if (response.status.code == this.constant.STATUS_OK) {
+          this.feedbackMsg = '';
+          this.isModalOpen = false;
+          this.modal.dismiss();
+          this.coreService.showToastMessage(
+            response["status"]["description"],
+            this.coreService.TOAST_SUCCESS
+          );
+        } else {
+          this.coreService.showToastMessage(
+            response["status"]["description"],
+            this.coreService.TOAST_ERROR
+          );
+        }
+      });
+    }
+  }
+
+  getFAQList() {
+    let request: any = {
+      path: "auth/faq",
+      isAuth: true,
+    };
+      this.apiService.get(request).subscribe((response: any) => {
+        this.faqList = response?.data;
+      });
+  }
+    
 
 }
