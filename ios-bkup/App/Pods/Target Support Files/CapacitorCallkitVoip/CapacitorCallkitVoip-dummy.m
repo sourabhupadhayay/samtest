59c58413d85db11b702f46528bfc6e3593c2241f
Only Mac <onlymac@Onlys-MacBook-Pro.local>
2023-04-04 14:05:29 +0530

    [AK] remove ios backup folders

--- ios-bkup/App/Pods/Target Support Files/CapacitorCallkitVoip/CapacitorCallkitVoip-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_CapacitorCallkitVoip : NSObject
-@end
-@implementation PodsDummy_CapacitorCallkitVoip
-@end
--- ios-bkup/App/Pods/Target Support Files/FreshaCapacitorPluginApplepay/FreshaCapacitorPluginApplepay-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_FreshaCapacitorPluginApplepay : NSObject
-@end
-@implementation PodsDummy_FreshaCapacitorPluginApplepay
-@end
